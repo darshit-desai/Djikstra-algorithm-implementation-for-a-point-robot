49a20bd70dabb82ab2e90b6f0820f03a554b2325
Darshit Desai <36150235+darshit-desai@users.noreply.github.com>
2023-03-11 19:34:58 -0500

    Initial version of the algorithm that works
    The algorithm works successfully in this code but time complexity optimization, functional programming and the code to check whether the goal can be reached or not is left

--- code.py
+++ code.py
@@ -35,16 +35,17 @@ running = True
 
 
 
-start_node = (5,5)
-goal_node = (400,125)
+start_node = (550,30)
+goal_node = (6,6)
 Node_i = 0
 Parent_Node_i = -1
 initial_c2c = 0
 node_state=[initial_c2c, Node_i, Parent_Node_i, start_node]
-
+index=None
 Open_list = PriorityQueue()
 Open_list.put(node_state)
 Closed_list = {}
+
 
 def gen_up(node,nc2c):
 	nnode = (node[0]+0,node[1]-1)
@@ -78,122 +79,161 @@ def gen_down_right(node, nc2c):
 	nnode = (node[0]+1,node[1]+1)
 	cost = nc2c + 1.4
 	return nnode, cost
-
+Parent_Node_i+=1
+gni=None
 while (Open_list.empty()==False):
 	
 	nodegen = Open_list.get()
 	nodecoord = nodegen[-1]
 	nodec2c = nodegen[0]
-	Closed_list[Node_i]=[nodec2c,nodegen[2],nodecoord]
-	print(Closed_list)
+	Closed_list[nodegen[1]]=[nodec2c,nodegen[2],nodecoord]
+	# print("My current parent in open list", nodegen)
+	# print("The whole Open list",Open_list)
 	if(nodecoord==goal_node):
+		print("Woohoo we reached the goal")
+		gni=nodegen[1]
+		gnc=nodecoord
 		break
-	Parent_Node_i+=1
+	Parent_Node_i=nodegen[1]
 	new_node_up, new_node_cost_up = gen_up(nodecoord, nodec2c)
-	
 	if (screen.get_at(new_node_up)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_up, Closed_list.items()))):
-			if new_node_up not in [tup[3] for tup in Open_list]:
+		if not(any(val[-1] == new_node_up for val in Closed_list.values())):
+			if new_node_up not in [tup[3] for tup in Open_list.queue]:
 				Node_i+=1
 				temp_state = [new_node_cost_up, Node_i, Parent_Node_i, new_node_up]
+				# print("Node up generated node",temp_state)
 				Open_list.put(temp_state)
 			else:
-				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_up)
-				if (new_node_cost_up<Open_list[index][0]):
-					Open_list[index][0]=new_node_cost_up
-					Open_list[index][2]=nodegen[2]
-	
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_up)
+				if (round(new_node_cost_up,1)<round(Open_list.queue[index][0],1)):
+					# print("New Cost rounded",new_node_cost_up)
+					Open_list.queue[index][0]=new_node_cost_up
+					Open_list.queue[index][2]=nodegen[2]
+					# print("*****I went inside and modified during up",Open_list.queue[index])
+	# print(Open_list)
 	new_node_down, new_node_cost_down = gen_down(nodecoord, nodec2c)
 	if (screen.get_at(new_node_down)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_down, Closed_list.items()))):
-			if new_node_down not in [tup[3] for tup in Open_list]:
+		if not(any(val[-1] == new_node_down for val in Closed_list.values())):
+			if new_node_down not in [tup[3] for tup in Open_list.queue]:
 				Node_i+=1
 				temp_state = [new_node_cost_down, Node_i, Parent_Node_i, new_node_down]
+				# print("Node down generated node",temp_state)
 				Open_list.put(temp_state)
 			else:
-				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_down)
-				if (new_node_cost_down<Open_list[index][0]):
-					Open_list[index][0]=new_node_cost_down
-					Open_list[index][2]=nodegen[2]
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_down)
+				if (round(new_node_cost_down,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_down
+					Open_list.queue[index][2]=nodegen[2]
+					# print("*****I went inside and modified during down",Open_list.queue[index])
 
 	new_node_left, new_node_cost_left = gen_left(nodecoord, nodec2c)
 	if (screen.get_at(new_node_left)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_left, Closed_list.items()))):
-			if new_node_left not in [tup[3] for tup in Open_list]:
+		if not(any(val[-1] == new_node_left for val in Closed_list.values())):
+			if new_node_left not in [tup[3] for tup in Open_list.queue]:
 				Node_i+=1
 				temp_state = [new_node_cost_left, Node_i, Parent_Node_i, new_node_left]
+				# print("Node left generated node",temp_state)
 				Open_list.put(temp_state)
 			else:
-				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_left)
-				if (new_node_cost_left<Open_list[index][0]):
-					Open_list[index][0]=new_node_cost_left
-					Open_list[index][2]=nodegen[2]
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_left)
+				if (round(new_node_cost_left,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_left
+					Open_list.queue[index][2]=nodegen[2]
+					# print("*****I went inside and modified during left",Open_list.queue[index])
 
 	new_node_right, new_node_cost_right = gen_right(nodecoord, nodec2c)
 	if (screen.get_at(new_node_right)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_right, Closed_list.items()))):
-				if new_node_right not in [tup[3] for tup in Open_list]:
-					Node_i+=1
-					temp_state = [new_node_cost_right, Node_i, Parent_Node_i, new_node_right]
-					Open_list.put(temp_state)
-				else:
-					index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_right)
-					if (new_node_cost_right<Open_list[index][0]):
-						Open_list[index][0]=new_node_cost_right
-						Open_list[index][2]=nodegen[2]
+		if not(any(val[-1] == new_node_right for val in Closed_list.values())):
+			if new_node_right not in [tup[3] for tup in Open_list.queue]:
+				Node_i+=1
+				temp_state = [new_node_cost_right, Node_i, Parent_Node_i, new_node_right]
+				# print("Node right generated node",temp_state)
+				Open_list.put(temp_state)
+			else:
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_right)
+				if (round(new_node_cost_right,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_right
+					Open_list.queue[index][2]=nodegen[2]
+					# print("*****I went inside and modified during right",Open_list.queue[index])
 
 	new_node_up_left, new_node_cost_up_left = gen_up_left(nodecoord, nodec2c)
 	if (screen.get_at(new_node_up_left)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_up_left, Closed_list.items()))):
-				if new_node_up_left not in [tup[3] for tup in Open_list]:
-					Node_i+=1
-					temp_state = (new_node_cost_up_left, Node_i, Parent_Node_i, new_node_up_left)
-					Open_list.put(temp_state)
-				else:
-					index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_up_left)
-					if (new_node_cost_up_left<Open_list[index][0]):
-						Open_list[index][0]=new_node_cost_up_left
-						Open_list[index][2]=nodegen[2]
+		if not(any(val[-1] == new_node_up_left for val in Closed_list.values())):
+			if new_node_up_left not in [tup[3] for tup in Open_list.queue]:
+				Node_i+=1
+				temp_state = [new_node_cost_up_left, Node_i, Parent_Node_i, new_node_up_left]
+				# print("Node up left generated node",temp_state)
+				Open_list.put(temp_state)
+			else:
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_up_left)
+				if (round(new_node_cost_up_left,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_up_left
+					Open_list.queue[index][2]=nodegen[2]
+					# print("*****I went inside and modified during up left",Open_list.queue[index])
 
 	new_node_up_right, new_node_cost_up_right = gen_up_right(nodecoord, nodec2c)
 	if (screen.get_at(new_node_up_right)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_up_right, Closed_list.items()))):
-				if new_node_up_right not in [tup[3] for tup in Open_list]:
-					Node_i+=1
-					temp_state = [new_node_cost_up_right, Node_i, Parent_Node_i, new_node_up_right]
-					Open_list.put(temp_state)
-				else:
-					index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_up_right)
-					if (new_node_cost_up_right<Open_list[index][0]):
-						Open_list[index][0]=new_node_cost_up_right
-						Open_list[index][2]=nodegen[2]
+		if not(any(val[-1] == new_node_up_right for val in Closed_list.values())):
+			if new_node_up_right not in [tup[3] for tup in Open_list.queue]:
+				Node_i+=1
+				temp_state = [new_node_cost_up_right, Node_i, Parent_Node_i, new_node_up_right]
+				print("Node up right generated node",temp_state)
+				Open_list.put(temp_state)
+			else:
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_up_right)
+				if (round(new_node_cost_up_right,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_up_right
+					Open_list.queue[index][2]=nodegen[2]
+					print("*****I went inside and modified during up right", Open_list.queue[index])
 
 	new_node_down_left, new_node_cost_down_left = gen_down_left(nodecoord, nodec2c)
 	if (screen.get_at(new_node_down_left)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_down_left, Closed_list.items()))):
-				if new_node_down_left not in [tup[3] for tup in Open_list]:
-					Node_i+=1
-					temp_state = [new_node_cost_down_left, Node_i, Parent_Node_i, new_node_down_left]
-					Open_list.put(temp_state)
-				else:
-					index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_down_left)
-					if (new_node_cost_down_left<Open_list[index][0]):
-						Open_list[index][0]=new_node_cost_down_left
-						Open_list[index][2]=nodegen[2]
+		if not(any(val[-1] != new_node_down_left for val in Closed_list.values())):
+			if new_node_down_left not in [tup[3] for tup in Open_list.queue]:
+				Node_i+=1
+				temp_state = [new_node_cost_down_left, Node_i, Parent_Node_i, new_node_down_left]
+				print("Node down left generated node",temp_state)
+				Open_list.put(temp_state)
+			else:
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_down_left)
+				if (round(new_node_cost_down_left,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_down_left
+					Open_list.queue[index][2]=nodegen[2]
+					print("*****I went inside and modified during down left", Open_list.queue[index])
 
 	new_node_down_right, new_node_cost_down_right = gen_down_right(nodecoord, nodec2c)
 	if (screen.get_at(new_node_down_right)!=(255, 0, 0, 255)):
-		if not(any(map(lambda x:x[2]==new_node_down_right, Closed_list.items()))):
-				if new_node_down_right not in [tup[3] for tup in Open_list]:
-					Node_i+=1
-					temp_state = [new_node_cost_down_right, Node_i, Parent_Node_i, new_node_down_right]
-					Open_list.put(temp_state)
-				else:
-					index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list) if (x, y) == new_node_down_right)
-					if (new_node_cost_down_right<Open_list[index][0]):
-						Open_list[index][0]=new_node_cost_down_right
-						Open_list[index][2]=nodegen[2]
+		if not(any(val[-1] == new_node_down_right for val in Closed_list.values())):
+			if new_node_down_right not in [tup[3] for tup in Open_list.queue]:
+				Node_i+=1
+				temp_state = [new_node_cost_down_right, Node_i, Parent_Node_i, new_node_down_right]
+				print("Node down right generated node",temp_state)
+				Open_list.put(temp_state)
+			else:
+				index = next(i for i, (_, _, _, (x, y)) in enumerate(Open_list.queue) if (x, y) == new_node_down_right)
+				if (round(new_node_cost_down_right,1)<round(Open_list.queue[index][0],1)):
+					Open_list.queue[index][0]=new_node_cost_down_right
+					Open_list.queue[index][2]=nodegen[2]
+					print("******I went inside and modified during down right",Open_list.queue[index])
+	screen.set_at(nodecoord,(255,0,255))
+	pyg.display.update()
 	
+print(Closed_list)	
+path=[]
+# path.append(gnc)
+while gni!=-1:
+	path.append(Closed_list[gni][-1])
+	gni=Closed_list[gni][1]
+	print(1)
+path.reverse()
+print(path)
+# for valu in Closed_list.values():
+# 	screen.set_at(valu[-1],(255,0,255))
+# 	pyg.display.update()	
+for co in path:
+	screen.set_at(co,(0,0,0))
+	pyg.display.update()
+
 
 while running:
 	# for loop through the event queue
